508b3c2c0dbd754f5dfd0e076bb2875168c61779
BigBinary <vikasbishnoi@Vikas-MacBook-Pro.local>
2022-08-22 15:55:23 +0530

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module Granite
+  class Application < Rails::Application
+    # previous code if any
+
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
+  end
+end
