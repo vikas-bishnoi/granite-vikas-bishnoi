c1d474adb96194ea79e2dbafbd3a4e0f22dba831
Vikas Bishnoi <vikaskbm@gmail.com>
2022-08-23 23:40:01 +0530

    Added constraint to make title not nullable

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+    MAX_TITLE_LENGTH = 125
+    validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
 end
+sql = "INSERT INTO TASKS (id, created_at, updated_at) VALUES (30, '2019-02-07 10:01:47', '2019-02-07 10:01:47')"
