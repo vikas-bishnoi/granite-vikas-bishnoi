8e9c38f7422dc22cca50ce91b0a587dda4e408a8
Vikas Bishnoi <vikaskbm@gmail.com>
2022-08-24 17:50:03 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
-import { setAuthHeaders } from "apis/axios";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
 import Dashboard from "components/Dashboard";
 import PageLoader from "components/PageLoader";
@@ -10,9 +10,10 @@ import CreateTask from "components/Tasks/Create";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     initializeLogger();
-
+    registerIntercepts();
     setAuthHeaders(setLoading);
   }, []);
 
@@ -27,7 +28,6 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
         <Route exact component={CreateTask} path="/tasks/create" />
         <Route exact component={Dashboard} path="/dashboard" />
       </Switch>
--- config/webpack/alias.js
+++ config/webpack/alias.js
@@ -1,10 +1,11 @@
 module.exports = {
-    resolve: {
-      alias: {
-        apis: "src/apis",
-        common: "src/common",
-        components: "src/components",
-      },
+  resolve: {
+    alias: {
+      apis: "src/apis",
+      utils: "src/utils",
+      common: "src/common",
+      components: "src/components",
+      constants: "src/constants",
     },
-  };
-  
+  },
+};
