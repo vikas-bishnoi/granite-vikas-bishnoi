4006acc359037421111c4a1a24d38fb28ecf1eb0
Vikas Bishnoi <vikaskbm@gmail.com>
2022-08-26 01:08:00 +0530

    Added unit tests for Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,12 +3,13 @@
 class Task < ApplicationRecord
   RESTRICTED_ATTRIBUTES = %i[title task_owner_id assigned_user_id]
 
+  MAX_TITLE_LENGTH = 50
   enum progress: { pending: "pending", completed: "completed" }
   enum status: { unstarred: "unstarred", starred: "starred" }
 
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 50 }
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
   validates :slug, uniqueness: true
   validate :slug_not_changed
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  config.active_support.test_order = :alpha
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
