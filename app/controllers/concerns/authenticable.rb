a56654c65bd569f4924fd25f56d04aa55a19972d
Vikas Bishnoi <vikaskbm@gmail.com>
2022-09-05 22:27:27 +0530

    Added linux platform to lockfile

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Authenticable
   extend ActiveSupport::Concern
 
@@ -11,10 +13,13 @@ module Authenticable
       user_email = request.headers["X-Auth-Email"].presence
       auth_token = request.headers["X-Auth-Token"].to_s
       user = user_email && User.find_by!(email: user_email)
-      is_valid_token = auth_token && ActiveSupport::SecurityUtils.secure_compare(user.authentication_token, auth_token)
+      is_valid_token = auth_token && ActiveSupport::SecurityUtils.secure_compare(
+        user.authentication_token,
+        auth_token)
       if is_valid_token
         @current_user = user
       else
         respond_with_error(t("session.could_not_auth"), :unauthorized)
       end
     end
+end
